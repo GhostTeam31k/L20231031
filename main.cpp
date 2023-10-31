40f0316c299d3451aecfdf0038dec536728c5428
GhostTeam31k <148298741+GhostTeam31k@users.noreply.github.com>
2023-10-31 11:26:15 +0900

    Tetris

--- main.cpp
+++ main.cpp
@@ -2,14 +2,32 @@
 
 using namespace std;
 
-int main()
-{
-	int A = 1;
-	int B = 10;
-	int C = A + B;
+// int A,B의 값을 바꾸는 요건
 
-	cout << A+B+C << endl;
-	cout << C + C + C << " C의 3배의 합 " << endl;
+//이름은 같으나 다른 공간
+void Swap(int* A, int* B)  //*을써서 메모리 주소를 저장
+{
+	int Temp = *A;
+	*A = *B;
+	*B = Temp;
+}
+
+int main() //다른 공간에 있는 A ,B를 쳐다보게 하기
+{
+	int A = 10;
+	int B = 20;
+
+	Swap(&A, &B); //&를 써서 저장한 주소값을 입력
+
+	
+	cout << A << endl;
+	cout << B << endl;
 
 	return 0;
 }
+
+
+
+
+
+
